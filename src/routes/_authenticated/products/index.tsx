ec804c5bf3efe58f4409ac3d58fe3e945359da8c
terrtam <97931134+terrtam@users.noreply.github.com>
2026-02-10 00:26:30 -0500

    Update sidebar to warehouse pages and switch products to tasks template

--- src/routes/_authenticated/products/index.tsx
+++ src/routes/_authenticated/products/index.tsx
@@ -1,14 +1,20 @@
 import z from 'zod'
 import { createFileRoute } from '@tanstack/react-router'
 import { Products } from '@/features/products'
+import { priorities, statuses } from '@/features/products/data/data'
 
 const productsSearchSchema = z.object({
-  type: z
-    .enum(['all', 'connected', 'notConnected'])
+  page: z.number().optional().catch(1),
+  pageSize: z.number().optional().catch(10),
+  status: z
+    .array(z.enum(statuses.map((status) => status.value)))
     .optional()
-    .catch(undefined),
+    .catch([]),
+  priority: z
+    .array(z.enum(priorities.map((priority) => priority.value)))
+    .optional()
+    .catch([]),
   filter: z.string().optional().catch(''),
-  sort: z.enum(['asc', 'desc']).optional().catch(undefined),
 })
 
 export const Route = createFileRoute('/_authenticated/products/')({
